78accf90015100e8ce493e784905c4c073cf77fb
Andrii <andrey.knyazev1995@gmail.com>
2024-02-25 16:53:50 +0200

    upd

--- src/main/kotlin/com/knzv/khrsn/demo/repository/UserRequestLogRepository.kt
+++ src/main/kotlin/com/knzv/khrsn/demo/repository/UserRequestLogRepository.kt
@@ -3,4 +3,4 @@ package com.knzv.khrsn.demo.repository
 import com.knzv.khrsn.demo.entity.UserRequestLogEntity
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 
-interface UserRequestLogRepository : ElasticsearchRepository<UserRequestLogEntity, Long>
+interface UserRequestLogRepository : ElasticsearchRepository<UserRequestLogEntity, String>
